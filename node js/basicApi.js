c77d921222ea4cca10defff9d416f9771518f205
akashkhot12 <akashkhot03@gmail.com>
2023-12-04 11:35:45 +0530

    json file data

--- node js/basicApi.js
+++ node js/basicApi.js
@@ -1,25 +1,8 @@
 const http = require("http");
+const data = require("./data")
 http.createServer((req,res)=>{
 res.writeHead(200,{'content-type':'application\json'});
-res.write(JSON.stringify( {
-    "id": 1,
-    "title": "iPhone 9",
-    "description": "An apple mobile which is nothing like apple",
-    "price": 549,
-    "discountPercentage": 12.96,
-    "rating": 4.69,
-    "stock": 94,
-    "brand": "Apple",
-    "category": "smartphones",
-    "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-    "images": [
-      "https://i.dummyjson.com/data/products/1/1.jpg",
-      "https://i.dummyjson.com/data/products/1/2.jpg",
-      "https://i.dummyjson.com/data/products/1/3.jpg",
-      "https://i.dummyjson.com/data/products/1/4.jpg",
-      "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-    ]
-  }))
+res.write(JSON.stringify(data))
 res.end()
 }).listen(5000,()=>{
     console.log("server is started");
