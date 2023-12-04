16fa8347374a050f034776e9eff525a52f48c09b
akashkhot12 <akashkhot03@gmail.com>
2023-12-04 12:10:48 +0530

    basic api fetch

--- node js/basicApi.js
+++ node js/basicApi.js
@@ -1,11 +1,9 @@
 const http = require("http");
-const data = require("./data");
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "content-type": "applicationjson" });
+const data = require("./data")
+http.createServer((req,res)=>{
+    res.writeHead(200,{'content-type':'application\json'});
     res.write(JSON.stringify(data));
-    res.end();
-  })
-  .listen(5000, () => {
+    res.end()
+}).listen(5000,()=>{
     console.log("server is started");
-  });
+})
