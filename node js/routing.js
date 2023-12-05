f119ce92e94e6452573bf3c883f316f2ea55eca2
akashkhot12 <akashkhot03@gmail.com>
2023-12-05 09:57:09 +0530

    routing all json data get for if else statements

--- node js/routing.js
+++ node js/routing.js
@@ -1,6 +1,23 @@
 const http = require("http");
+const data = require("./data")
 
 http.createServer((req,res)=>{
     res.writeHead(200,{"content-type":"text/html"})
-    res.write()
+   
+
+    // add routes
+    const url = req.url;
+    if (url==="/profile") {
+        res.write("<h1> is akash khot !</h1>")
+        res.end()
+    }
+    else if(url === "/details"){
+        res.write(JSON.stringify(data))
+    }
+    else{
+        res.write("<h1>hello there !</h1>");
+        res.end();
+    }
+}).listen(5000,()=>{
+    console.log("server started on 5000 port");
 })
