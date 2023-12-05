e6df722daac31e08f8d9ebe2d52f68a587caafde
akashkhot12 <akashkhot03@gmail.com>
2023-12-05 10:13:31 +0530

    send option query

--- Express js/basicQuery.js
+++ Express js/basicQuery.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('',(req,res)=>{
     console.log("user input => " ,req.query.name);
-    res.send("hello this is my name ")
+    res.send("hello this is my name "+req.query.name)
 }).listen(5000,()=>{
     console.log("server is stared");
 })
