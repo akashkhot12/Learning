d610df844e45def9dde66f0cccd2b4fdb2c9d98a
akashkhot12 <akashkhot03@gmail.com>
2023-12-05 11:24:38 +0530

    public path

--- Express js/linkPublic.js
+++ Express js/linkPublic.js
@@ -1,8 +1,9 @@
-const express = require("express")
-const path = require("path")
-const app =express()
+const express = require("express");
+const path  = require("path")
+const app = express();
 
-const obj = path.join(__dirname,'public');
-app.use(express.static(obj));
+const publicPath = path.join(__dirname,'public')
+
+app.use(express.static(publicPath))
 
 app.listen(5000)
