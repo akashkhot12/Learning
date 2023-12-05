bf45075baa3b42ca5f4301a7fd74fcb6e0a8b028
akashkhot12 <akashkhot03@gmail.com>
2023-12-05 11:14:01 +0530

    link page

--- Express js/linkPage.js
+++ Express js/linkPage.js
@@ -5,6 +5,21 @@ app.get('',(req,res)=>{
     res.send("this is home page")
 })
 
+app.get('/name',(req,res)=>{
+    res.send(`this is name page 
+            <a href ="/">go to home page</a>`)
+})
+
+app.get('/details',(req,res)=>{
+    res.send("this is details page")
+})
+
+app.get('/input',(req,res)=>{
+    res.send(`"
+        <input type="text" value="${req.query.name}">
+        <button>click me</button>
+    `)
+})
 
 app.listen(5000,()=>{
     console.log("server is started");
