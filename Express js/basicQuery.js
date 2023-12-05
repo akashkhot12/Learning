9f32461beca3db5ca6a775eedd569ad9345e7d9c
akashkhot12 <akashkhot03@gmail.com>
2023-12-05 10:11:10 +0530

    using query

---
+++ Express js/basicQuery.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get('',(req,res)=>{
+    console.log("user input => " ,req.query.name);
+    res.send("hello this is my name ")
+}).listen(5000,()=>{
+    console.log("server is stared");
+})
