c1c54a289991ac255b896ffa1606003c494d8a35
akashkhot12 <akashkhot03@gmail.com>
2023-12-05 11:31:54 +0530

    using routes for getting html file path

--- Express js/linkPublic.js
+++ Express js/linkPublic.js
@@ -4,6 +4,14 @@ const app = express();
 
 const publicPath = path.join(__dirname,'public')
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
+
+app.get('/',(req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/home',(req,res)=>{
+    res.sendFile(`${publicPath}/home.html`)
+})
 
 app.listen(5000)
