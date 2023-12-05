23880d3d2687cecd40fae7076c94245b75ebc490
akashkhot12 <akashkhot03@gmail.com>
2023-12-05 09:57:40 +0530

    all forms are convert into a similar paths

--- node js/routing.js
+++ node js/routing.js
@@ -1,23 +1,22 @@
 const http = require("http");
-const data = require("./data")
+const data = require("./data");
 
-http.createServer((req,res)=>{
-    res.writeHead(200,{"content-type":"text/html"})
-   
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "content-type": "text/html" });
 
     // add routes
     const url = req.url;
-    if (url==="/profile") {
-        res.write("<h1> is akash khot !</h1>")
-        res.end()
+    if (url === "/profile") {
+      res.write("<h1> is akash khot !</h1>");
+      res.end();
+    } else if (url === "/details") {
+      res.write(JSON.stringify(data));
+    } else {
+      res.write("<h1>hello there !</h1>");
+      res.end();
     }
-    else if(url === "/details"){
-        res.write(JSON.stringify(data))
-    }
-    else{
-        res.write("<h1>hello there !</h1>");
-        res.end();
-    }
-}).listen(5000,()=>{
+  })
+  .listen(5000, () => {
     console.log("server started on 5000 port");
-})
+  });
