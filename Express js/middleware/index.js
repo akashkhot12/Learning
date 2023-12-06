14673ae362da18ee6103f67dc4696f682c759a5c
akashkhot12 <akashkhot03@gmail.com>
2023-12-06 12:42:31 +0530

    middleware use

--- Express js/middleware/index.js
+++ Express js/middleware/index.js
@@ -1,25 +1,26 @@
-const express =require("express");
+const express = require("express");
 const app = express();
 
-
-// middleware 
-const reqFilter = (req,res,next)=>{
-    console.log("this is next funtioning");
-    next();
+const filter = (req,res,next)=>{
+    if (!req.query.age) {
+        res.send("please provide your age")
+    } else if (req.query.age<18) {
+        res.send("your are under age")
+    }else{
+        next()
+    }
 }
 
-app.use(reqFilter)
+app.use(filter);
 
-app.get('',(req,res)=>{
-    res.send("this is home page")
+app.get('/',(req,res)=>{
+    res.send("<h1>this is home page</h1>")
 })
 
-app.get('/about',(req,res)=>{
-    res.send("this is about page")
+app.get('/user',(req,res)=>{
+    res.send("<h1>this is user page</h1>")
 })
 
-app.get('/male',(req,res)=>{
-    res.send("this is male page")
+app.listen(5000,()=>{
+    console.log("server is started");
 })
-
-app.listen(5000)
