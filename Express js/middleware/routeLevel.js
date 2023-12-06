b3f16b9fccd5571ce7fc4ec5610170260f447823
akashkhot12 <akashkhot03@gmail.com>
2023-12-06 13:02:09 +0530

    using flter

--- Express js/middleware/routeLevel.js
+++ Express js/middleware/routeLevel.js
@@ -1,26 +1,21 @@
-const express = require("express");
+const express = require ("express");
 const app = express();
 
-const filter = (req,res,next)=>{
-    if (!req.query.age) {
-        res.send("please provide your age")
-    } else if (req.query.age<18) {
-        res.send("your are under age")
-    }else{
-        next()
-    }
+const filter  = (req,res,next)=>{
+    
 }
 
-app.use(filter);
+app.use(filter)
 
-app.get('/',(req,res)=>{
-    res.send("<h1>this is home page</h1>")
+
+app.get("/",(req,res)=>{
+    res.send("<h1>this is home page </h1>")
 })
 
 app.get('/user',(req,res)=>{
-    res.send("<h1>this is user page</h1>")
+    res.send("<h1>this is user page </h1>")
 })
 
-app.listen(5000,()=>{
-    console.log("server is started");
+app.listen(7000,()=>{
+    console.log("server is started ");
 })
