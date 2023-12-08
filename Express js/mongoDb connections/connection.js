8f5d2102dfdefd8fbed52d8d2dcb8ed9de811f2d
akashkhot12 <akashkhot03@gmail.com>
2023-12-08 12:04:46 +0530

    adding database

--- Express js/mongoDb connections/connection.js
+++ Express js/mongoDb connections/connection.js
@@ -1,3 +1,16 @@
 const {MongoClient}=require("mongodb");
+
 // another way to require mongo db
 // const MongoClient = require("mongodb").MongoClient;
+
+// use database name for storing database name;
+const database = 'ecom'
+// create path for mongo 
+const url = 'mongodb://localhost:27017';
+
+const Client = new MongoClient(url);
+
+
+async function getData(){
+ let result = await Client.connect()
+}
