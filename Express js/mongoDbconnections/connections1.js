ab649460578f7c6349ea179a5b15f383fcca9053
akashkhot12 <akashkhot03@gmail.com>
2023-12-08 12:18:58 +0530

    adding node js with mongo db

---
+++ Express js/mongoDbconnections/connections1.js
@@ -0,0 +1,13 @@
+const {MongoClient}=require("mongodb");
+ const url = "mongodb://localhost:27017"
+const client = new MongoClient(url);
+const database  = 'ecom';
+
+async function getdata(){
+    let result = await client.connect();
+    let db = result.db(database);
+    let collections = db.collection('products');
+    let response = await collections.find().toArray();
+    console.log(response);
+}
+getdata();
