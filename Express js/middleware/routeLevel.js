24ccbc3cfa8c887b7a5860b5d0381faea14b4ddf
akashkhot12 <akashkhot03@gmail.com>
2023-12-08 10:25:27 +0530

    all project wrking

--- Express js/middleware/routeLevel.js
+++ Express js/middleware/routeLevel.js
@@ -13,14 +13,14 @@ const filter  = (req,res,next)=>{
     }
 }
 
-app.use(filter)
+
 
 
 app.get("/",(req,res)=>{
     res.send("<h1>this is home page </h1>")
 })
 
-app.get('/user',(req,res)=>{
+app.get('/user',filter,(req,res)=>{
     res.send("<h1>this is user page </h1>")
 })
 
