7fbf51b4a26dd8cd8562c06ca517b5ba3db2e33e
akashkhot12 <akashkhot03@gmail.com>
2023-12-09 09:53:16 +0530

    inserted data succefully

--- Express js/CRUD/insert.js
+++ Express js/CRUD/insert.js
@@ -3,28 +3,10 @@ const dbconnect = require("../mongoDbconnections/connect")
 
 // this code is single inserted data in mongo db
 
-// const insertdata = async()=>{
-//     let data = await dbconnect();
-//     const result = await data.insertOne(
-//         {name:"oppo 20",brand:"oppo",category:"mobile",price:220}
-//     )
-//     // console.log(result);
-//     if (result.acknowledged) {
-//         console.log("data inserted suceesfully");
-//     }
-// }
-
-// insertdata()
-
-
-// this code is multiple data inserted on mongo db
 const insertdata = async()=>{
     let data = await dbconnect();
-    const result = await data.insertMany([
-        {name:"oppo 10",brand:"oppo",category:"mobile",price:220},
-        {name:"vivo 10",brand:"vivo",category:"mobile",price:220},
-        {name:"samsung 10",brand:"sansung",category:"mobile",price:2120},
-    ]
+    const result = await data.insertOne(
+        {name:"oppo 20",brand:"oppo",category:"mobile",price:220}
     )
     // console.log(result);
     if (result.acknowledged) {
@@ -33,6 +15,24 @@ const insertdata = async()=>{
 }
 
 insertdata()
+
+
+// this code is multiple data inserted on mongo db
+// const insertdata = async()=>{
+//     let data = await dbconnect();
+//     const result = await data.insertMany([
+//         {name:"oppo 10",brand:"oppo",category:"mobile",price:220},
+//         {name:"vivo 10",brand:"vivo",category:"mobile",price:220},
+//         {name:"samsung 10",brand:"sansung",category:"mobile",price:2120},
+//     ]
+//     )
+//     // console.log(result);
+//     if (result.acknowledged) {
+//         console.log("data inserted suceesfully");
+//     }
+// }
+
+// insertdata()
 
 
 
