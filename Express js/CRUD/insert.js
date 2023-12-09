041cf2122a8a18f16053c39d4f0c3bf606533c9c
akashkhot12 <akashkhot03@gmail.com>
2023-12-09 09:46:49 +0530

    one inserted data

--- Express js/CRUD/insert.js
+++ Express js/CRUD/insert.js
@@ -3,9 +3,12 @@ const dbconnect = require("../mongoDbconnections/connect")
 const insertdata = async()=>{
     let data = await dbconnect();
     const result = await data.insertOne(
-        {name:"nokia 120",brand:"nokia",category:"mobile",price:1220}
+        {name:"oppo 20",brand:"oppo",category:"mobile",price:220}
     )
-    console.log("inserted successfully");
+    // console.log(result);
+    if (result.acknowledged) {
+        console.log("data inserted suceesfully");
+    }
 }
 
 insertdata()
