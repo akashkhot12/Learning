c18f8978853f9f987edc79dbe8408c41a2821efd
akashkhot12 <akashkhot03@gmail.com>
2023-12-09 10:50:59 +0530

    product connection

--- Express js/mongoDbconnections/connect.js
+++ Express js/mongoDbconnections/connect.js
@@ -10,3 +10,16 @@ async function getConnections(){
 }
 
 module.exports = getConnections;
+
+
+
+const{MongoClient}=require("mongodb");
+const url = "mongodb://localhost:27017"
+const client = new MongoClient(url);
+const database  = "ecom"
+
+async function getConnections(){
+    let result = await client.connect();
+    let db  = result.db(database)
+    return db.collection('procts')
+}
