6d77de29fcac7501f6bb7a7b6fbed17e9344d889
akashkhot12 <akashkhot03@gmail.com>
2023-12-09 09:49:30 +0530

    multiple data inserted

--- Express js/CRUD/insert.js
+++ Express js/CRUD/insert.js
@@ -1,9 +1,30 @@
 const dbconnect = require("../mongoDbconnections/connect")
 
+
+// this code is single inserted data in mongo db
+
+// const insertdata = async()=>{
+//     let data = await dbconnect();
+//     const result = await data.insertOne(
+//         {name:"oppo 20",brand:"oppo",category:"mobile",price:220}
+//     )
+//     // console.log(result);
+//     if (result.acknowledged) {
+//         console.log("data inserted suceesfully");
+//     }
+// }
+
+// insertdata()
+
+
+// this code is multiple data inserted on mongo db
 const insertdata = async()=>{
     let data = await dbconnect();
-    const result = await data.insertOne(
-        {name:"oppo 20",brand:"oppo",category:"mobile",price:220}
+    const result = await data.insertMany([
+        {name:"oppo 10",brand:"oppo",category:"mobile",price:220},
+        {name:"vivo 10",brand:"vivo",category:"mobile",price:220},
+        {name:"samsung 10",brand:"sansung",category:"mobile",price:2120},
+    ]
     )
     // console.log(result);
     if (result.acknowledged) {
@@ -12,3 +33,7 @@ const insertdata = async()=>{
 }
 
 insertdata()
+
+
+
+
