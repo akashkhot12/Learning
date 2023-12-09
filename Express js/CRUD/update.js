fdfe0fc4d20b19898241893176415c56d98867d1
akashkhot12 <akashkhot03@gmail.com>
2023-12-09 10:08:48 +0530

    update process

--- Express js/CRUD/update.js
+++ Express js/CRUD/update.js
@@ -4,8 +4,21 @@ const dbConnect = require("../mongoDbconnections/connect");
 const update = async()=>{
     let data = await dbConnect();
     const result = await data.updateOne(
-        {
-            
-        }
+        {name:"m 40"},{$set:{name: "samsung m40 "}}
         )
+        console.log(result);
 }
+update()
+
+
+
+// update many
+
+// const update = async()=>{
+//     let data = await dbConnect();
+//     const result = await data.updateMany(
+//         {name:"m 40"},{$set:{name: "samsung m40 "}}
+//         )
+//         console.log(result);
+// }
+// update()
