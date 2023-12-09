d780053ef32cdb60dbb75929bd55391c98f8e8b0
akashkhot12 <akashkhot03@gmail.com>
2023-12-09 11:28:04 +0530

    crud operations

--- Express js/CRUD/crudOperation.js
+++ Express js/CRUD/crudOperation.js
@@ -1,58 +1,56 @@
-const dbConnect = require("../mongoDbconnections/connect")
-
+const dbConnect = require("../mongoDbconnections/connect");
 
 // get data
 
-const getData = async()=>{
-    let data = await dbConnect();
-    const result = await data.find().toArray();
-    if (!result) {
-        console.log("something wrong");
-    }
-    else{
-        console.log(result);
-    }
-}
+const getData = async () => {
+  let data = await dbConnect();
+  const result = await data.find().toArray();
+  if (!result) {
+    console.log("something wrong");
+  } else {
+    console.log(result);
+  }
+};
 
 // getData()
 
-
 // insert data
 
-const insertdata = async()=>{
-    let data = await dbConnect();
-    const result = await data.insertOne(
-        {name:"lava 1",brand:"lava",category:"mobile",price:200}
-        )
-        if (result.acknowledged) {
-            console.log("data insert successfully");
-        }
-}
+const insertdata = async () => {
+  let data = await dbConnect();
+  const result = await data.insertOne({
+    name: "lava 1",
+    brand: "lava",
+    category: "mobile",
+    price: 200,
+  });
+  if (result.acknowledged) {
+    console.log("data insert successfully");
+  }
+};
 // insertdata()
 
-
 // update data
-const updatedata = async()=>{
-    let data = await dbConnect();
-    const result = await data.updateOne(
-        {name:"lava 1"},{$set:{name:"lava 10"}}
-    )
-    if (result.acknowledged) {
-        console.log("data update successfully");
-    }
-}
+const updatedata = async () => {
+  let data = await dbConnect();
+  const result = await data.updateOne(
+    { name: "lava 1" },
+    { $set: { name: "lava 10" } }
+  );
+  if (result.acknowledged) {
+    console.log("data update successfully");
+  }
+};
 
 // updatedata()
 
 // delete data
 
-const deleteData = async()=>{
-    let data = await dbConnect();
-    const result = await data.deleteOne(
-        {name:"lava 10"}
-    )
-    if (result.acknowledged) {
-        console.log("data delete successfully");
-    }
-}
-deleteData()
+const deleteData = async () => {
+  let data = await dbConnect();
+  const result = await data.deleteOne({ name: "lava 10" });
+  if (result.acknowledged) {
+    console.log("data delete successfully");
+  }
+};
+deleteData();
