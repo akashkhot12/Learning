6383ddea46d3243486a56bdc72d33e95f6e207f3
akashkhot12 <akashkhot03@gmail.com>
2023-12-09 12:40:14 +0530

    bug fix

--- Express js/API/api.js
+++ Express js/API/api.js
@@ -2,11 +2,28 @@ const express  = require("express");
 const dbConnect = require("../mongoConnect");
 const app = express();
 
+app.use(express.json());
+
 
 app.get('/',async(req,res)=>{
    let data = await dbConnect();
    const result = await data.find().toArray()
    res.send(result)
+})
+
+app.post('/',async(req,res)=>{
+    let data = await dbConnect();
+    const result  = await data.insertOne(req.body)
+    res.send(result)
+})
+
+app.put('/:name',async(req,res)=>{
+    let data = await dbConnect();
+    const result = data.updateOne(
+        {name:req.params.name},
+        {$set:req.body}
+    );
+    res.send({result:"updated"})
 })
 
 app.listen(5000)
