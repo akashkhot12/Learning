51d85057673049d2dc85d390186c8faed6e2dfe7
akashkhot12 <akashkhot03@gmail.com>
2023-12-11 11:27:16 +0530

    mongoose

--- Express js/mongoose/index1.js
+++ Express js/mongoose/index1.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const main = async ()=>{
     await mongoose.connect("mongodb://localhost:27017/ecom");
-    l const productScehma = new mongoose.Schema({
+    const productScehma = new mongoose.Schema({
         name:String,
         age:Number
     })
