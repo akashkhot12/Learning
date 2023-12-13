f5e03b6556302b2e85dc9cbce834479f64431198
akashkhot12 <akashkhot03@gmail.com>
2023-12-13 12:56:21 +0530

    put method

--- Express js/mySql/getApi.js
+++ Express js/mySql/getApi.js
@@ -1,8 +1,41 @@
 const express = require('express');
+const con = require('./connection')
 const app = express();
 
-app.get('/list',(req,res)=>{
+app.use(express.json());
 
+
+app.get('/list',(req,res)=>{
+con.query("select * from users",(err,result)=>{
+    if (err) {
+        res.send(err)
+    }
+    else{
+        res.send(result)
+    }
 })
+})
+
+
+app.post('/',(req,res)=>{
+    // const data = {name:"akash",password:"Akash",user_type:"visitors"}; => for hardcoded data
+    const con = req.body;
+    con.query("insert into users SET ?",data,(error,result,feilds)=>{
+        if (error) {
+            res.send(error)
+        }
+        res.send(result)
+    })
+})
+
+app.put("/:id",(req,resp)=>{
+    const data= [req.body.name,req.body.password,req.body.user_type,req.params.id];
+    con.query("UPDATE users SET name = ?, password = ?, user_type = ? WHERE id = ?",
+    data,(error,results,fields)=>{
+      if(error) throw error;
+      resp.send(results)
+    })
+   
+  })
 
 app.listen(5000)
