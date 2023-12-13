66e8a7ec4964fe4917a25e0da55e0eddcbb31f41
akashkhot12 <akashkhot03@gmail.com>
2023-12-13 09:22:13 +0530

    crud eoperation completed

--- Express js/mongoose/crud.js
+++ Express js/mongoose/crud.js
@@ -1,14 +1,35 @@
 const mongoose = require("mongoose");
 
-const main  = async()=>{
-    await mongoose.connect("mongodb://localhost:27017/ecom");
-    const productScehma = new mongoose.Schema({
-        name:String,
-        price:Number
-    })
-    const productModel = mongoose.model('products',productScehma)
-    let data = new productModel({name:"sony",price:288})
+
+mongoose.connect("mongodb://localhost:27017/ecom");
+const productScehma = new mongoose.Schema({
+    name:String,
+    brand:String,
+    category:String,
+    price:Number
+})
+const saveInDB  = async()=>{
+   
+    const product = mongoose.model('products',productScehma)
+    let data = new product({name:"sony 80",brand:"sony", category:"mobile",price:288})
     let result = await data.save();
     console.log(result);
 }
-main()
+ 
+// saveInDB()
+
+const updateData = async()=>{
+    const product = mongoose.model('products',productScehma)
+    let data = await product.updateOne(
+        {name:"chetna bhanarkar"},{$set:{name:"errission"}}
+    )
+    console.log(data);
+}
+// updateData()
+
+const deleteData = async()=>{
+    const product = mongoose.model('products',productScehma);
+    let data = await product.deleteOne({name:"sony"})
+    console.log(data);
+}
+deleteData()
