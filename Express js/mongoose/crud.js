e2917a6ab9053182fcd71c6e546179084b586398
akashkhot12 <akashkhot03@gmail.com>
2023-12-13 08:56:59 +0530

    crud operation in mongoose db connect

---
+++ Express js/mongoose/crud.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const main  = async()=>{
+    await mongoose.connect("mongodb://localhost:27017/ecom");
+    const productScehma = new mongoose.Schema({
+        name:String,
+        age:Number
+    })
+    const productModel = mongoose.model('products',productScehma)
+    let data = new productModel({name:"chetna bhanarkar",age:12})
+    let result = await data.save();
+    console.log(result);
+}
+main()
