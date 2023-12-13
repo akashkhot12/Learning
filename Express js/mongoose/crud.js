d3966fe523aa9798690b68056e5070a8a2bae38b
akashkhot12 <akashkhot03@gmail.com>
2023-12-13 08:59:05 +0530

    adding value

--- Express js/mongoose/crud.js
+++ Express js/mongoose/crud.js
@@ -4,10 +4,10 @@ const main  = async()=>{
     await mongoose.connect("mongodb://localhost:27017/ecom");
     const productScehma = new mongoose.Schema({
         name:String,
-        age:Number
+        price:Number
     })
     const productModel = mongoose.model('products',productScehma)
-    let data = new productModel({name:"chetna bhanarkar",age:12})
+    let data = new productModel({name:"sony",price:288})
     let result = await data.save();
     console.log(result);
 }
