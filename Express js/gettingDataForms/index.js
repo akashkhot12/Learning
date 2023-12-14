ea698d72faaf5ee01057f697ae476c4487d0cb8e
akashkhot12 <akashkhot03@gmail.com>
2023-12-14 10:19:00 +0530

    all work is completed

--- Express js/gettingDataForms/index.js
+++ Express js/gettingDataForms/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./model/user');
+const fs = require('fs')
 const app = express();
 
+
+// middleware
+
+app.use(express.urlencoded({extended:true}))
+
+
+// connect database
 
 mongoose.connect("mongodb://localhost:27017/contact").then(()=>{
     console.log("database connected");
@@ -11,8 +19,22 @@ mongoose.connect("mongodb://localhost:27017/contact").then(()=>{
     console.log("database can't be connect");
 })
 
+
+// getting html form detail to send database 
+
+app.post('/',async(req,res)=>{
+    const userData = new User(req.body);
+    await userData.save();
+    let a = fs.readFileSync('submit.html')
+    res.send(a.toString());
+})
+
+
+// listening routes
+
 app.get('/',(req,res)=>{
-    res.send("hello start")
+    let a = fs.readFileSync('index.html')
+    res.send(a.toString());
 })
 
 app.listen(5000,()=>{
