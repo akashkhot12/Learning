f09047e292e2a1224a4c5d16e971023beb3618e0
akashkhot12 <akashkhot03@gmail.com>
2023-12-14 09:45:21 +0530

    create model schema

---
+++ Express js/gettingDataForms/model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userData = new mongoose.Schema({
+    name:{type:String},
+    email:{type:String},
+    compony:{type:String},
+    contact:{type:Number}
+})
+
+const User = mongoose.model("User",userData)
