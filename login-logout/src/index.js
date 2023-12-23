28adf4d309b673f24b7a39ba473f9259de8a58dd
akashkhot12 <akashkhot03@gmail.com>
2023-12-23 09:43:04 +0530

    adding json

--- login-logout/src/index.js
+++ login-logout/src/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const quates = require('./dummy.json')
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+app.get('/q',(req,res)=>{
+    res.json(quates)
+})
 
 app.listen(5000,()=>{
     console.log("server start on 5000 port");
