fc050c7c8b13dc019ef123f41cb09dd1b506ef52
akashkhot12 <akashkhot03@gmail.com>
2023-12-24 12:05:25 +0530

    singin

--- login-logout/src/controllers/userController.js
+++ login-logout/src/controllers/userController.js
@@ -37,8 +37,28 @@ const signup = async (req, res) => {
 
 }
 
-const signin = (req, res) => {
+const signin = async(req, res) => {
+    const{email,password} = req.body;
 
+    try {
+        const existingUser = await userModel.findOne({ email: email });
+        if (!existingUser) {
+            return res.status(404).json({ message: "User not found" }); 
+        }
+
+        const matchPassword = await bcrypt.compare(password,existingUser.password);
+
+        if (!matchPassword) {
+            return res.status(400).json({message:"invalid credentials"});
+        }
+        
+        const token = jwt.sign({email:existingUser.email,id:existingUser._id},SECRET_KEY);
+        res.status(201).json({user:existingUser,token:token})
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"something went wrong"});
+    }
 }
 
 module.exports = { signup, signin };
--- login-logout/src/index.js
+++ login-logout/src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const quates = require('./dummy.json');
 const userRouter = require('./routes/userRoutes');
 const noteRouter = require('./routes/noteRoutes');
+
 const mongoose = require('mongoose');
 
 
-app.use(express.json);
+app.use(express.json());
+
 app.use('/users', userRouter);
 app.use('/notes', noteRouter)
 
