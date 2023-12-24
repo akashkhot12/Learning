b373b412d96f540492419c038edabbcec8954965
akashkhot12 <akashkhot03@gmail.com>
2023-12-24 11:03:36 +0530

    formated

--- login-logout/src/index.js
+++ login-logout/src/index.js
@@ -3,22 +3,22 @@ const app = express();
 const quates = require('./dummy.json');
 const userRouter = require('./routes/userRoutes');
 const noteRouter = require('./routes/noteRoutes');
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 
 
 
-app.use('/users',userRouter);
-app.use('/notes',noteRouter)
+app.use('/users', userRouter);
+app.use('/notes', noteRouter)
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-mongoose.connect('mongodb+srv://Akash:Akash3975@loginlogout.2erou3k.mongodb.net/?retryWrites=true&w=majority').then(()=>{
-    app.listen(5000,()=>{
+mongoose.connect('mongodb+srv://Akash:Akash3975@loginlogout.2erou3k.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    app.listen(5000, () => {
         console.log("server start on 5000 port");
     })
-}).catch((error)=>{
+}).catch((error) => {
     console.log(error);
 })
 
