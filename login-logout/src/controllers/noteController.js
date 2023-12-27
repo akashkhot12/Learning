3852511862b8e56529af9190195a18c6680647ba
akashkhot12 <akashkhot03@gmail.com>
2023-12-27 11:26:57 +0530

    add notes

--- login-logout/src/controllers/noteController.js
+++ login-logout/src/controllers/noteController.js
@@ -1,20 +1,38 @@
 const noteModel = require('../models/notes');
 
-const createNote = (req,res)=>{
-console.log(req.userId);
+const createNote = async (req, res) => {
+    const { title, description } = req.body;
+    const newNote = new noteModel({
+        title: title,
+        decription: description,
+        userId: req.userId
+    });
+    try {
+        await newNote.save();
+        res.status(201).json(newNote)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ messege: "something went wrong" });
+    }
 }
 
-const updateNote = (req,res)=>{
-    
+const updateNote = (req, res) => {
+
 }
 
-const deleteNote = (req,res)=>{
-    
+const deleteNote = (req, res) => {
+
 }
 
 
-const getNote = (req,res)=>{
-    
+const getNote = async (req, res) => {
+    try {
+        const notes = await noteModel.find({ userId: req.userId });
+        res.status(200).json(notes)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ messege: "something went wrong" });
+    }
 }
 
 module.exports = {
