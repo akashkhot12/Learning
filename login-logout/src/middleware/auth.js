f97cd8769ae2d4c1846f7347ed54da600590b737
akashkhot12 <akashkhot03@gmail.com>
2023-12-27 10:31:38 +0530

    note router

--- login-logout/src/middleware/auth.js
+++ login-logout/src/middleware/auth.js
@@ -16,9 +16,11 @@ const auth = (req,res,next)=>{
         }
 
         next()
-        
+
     } catch (error) {
         console.log(error);
         res.status(401).json({messege:"Unauthorized User"})
     }
 }
+
+module.exports = auth;
