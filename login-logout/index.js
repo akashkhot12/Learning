09f745f876b45d639a75025254a4edfde45e4cc0
akashkhot12 <akashkhot03@gmail.com>
2023-12-27 09:07:01 +0530

    result true

--- login-logout/index.js
+++ login-logout/index.js
@@ -7,6 +7,7 @@ const password = 'Akash123';
 // create function
 const passkey  = async()=>{
     const hashedPassword =await bcrypt.hash(password,10);
+    console.log(hashedPassword);
     const matchPassword = await bcrypt.compare(password, hashedPassword);
     console.log(matchPassword);
 }
