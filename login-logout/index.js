b5a7deb8a505b9e1d4e17891bf4bf0f6bd0362a4
akashkhot12 <akashkhot03@gmail.com>
2023-12-27 09:03:14 +0530

    compare assword is true

--- login-logout/index.js
+++ login-logout/index.js
@@ -1,23 +1,19 @@
 const express  = require('express')
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const SECRET_KEY = "NOTESAPI";
+
 
 
 const password = 'Akash123';
-let store 
 // create function
 const passkey  = async()=>{
-    const hashedPassword =await bcrypt.hash(password,10)
-     store = hashedPassword;
-     console.log(store);
+    const hashedPassword =await bcrypt.hash(password,10);
+    const matchPassword = await bcrypt.compare(password, hashedPassword);
+    console.log(matchPassword);
 }
 passkey();
-console.log(store);
 
-const compareKey = async()=>{
-    const matchPassword = await bcrypt.compare(password, store);
-}
+
+
 
 
 
