adcaa34b18df98abe8be76585f299ea1650c5134
akashkhot12 <akashkhot03@gmail.com>
2023-12-27 08:51:30 +0530

    check password

--- login-logout/index.js
+++ login-logout/index.js
@@ -1,4 +1,19 @@
-const userModel = require('../models/users');
+const express  = require('express')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = "NOTESAPI";
+
+
+const password = 'Akash123';
+
+const passkey  = async()=>{
+    const hashedPassword =await bcrypt.hash(password,10)
+    console.log(hashedPassword);
+}
+passkey()
+
+if (condition) {
+    
+} else {
+    
+}
