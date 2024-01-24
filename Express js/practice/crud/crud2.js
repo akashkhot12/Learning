af5cc433b7425e63751629a4828d8c3e684ded40
akashkhot12 <akashkhot03@gmail.com>
2024-01-24 10:36:26 +0530

    complete all crud operations

--- Express js/practice/crud/crud2.js
+++ Express js/practice/crud/crud2.js
@@ -1,30 +1,33 @@
 const mongodb = require("../crud/connection2");
 
-const insertData = async()=>{
-    let result = await mongodb();
-    let insert = await result.insertOne({product:"light"});
-    console.log(insert);
-}
+const insertData = async () => {
+  let result = await mongodb();
+  let insert = await result.insertOne({ product: "light" });
+  console.log(insert);
+};
 // insertData();
 
-const readData = async()=>{
-    let result = await mongodb();
-    let read = await result.find().toArray();
-    console.log(read);
-}
+const readData = async () => {
+  let result = await mongodb();
+  let read = await result.find().toArray();
+  console.log(read);
+};
 
 // readData()
 
-const updateData = async()=>{
-    let result = await mongodb();
-    let update = await result.updateOne({product:"light"},{$set:{product:"bulp"}});
-    console.log(update);
-}
+const updateData = async () => {
+  let result = await mongodb();
+  let update = await result.updateOne(
+    { product: "light" },
+    { $set: { product: "bulp" } }
+  );
+  console.log(update);
+};
 // updateData();
 
-const deleteData = async()=>{
-    let result = await mongodb();
-    let delet = await result.deleteOne({product:"bulp"});
-    console.log(delet);
-}
-deleteData()
+const deleteData = async () => {
+  let result = await mongodb();
+  let delet = await result.deleteOne({ product: "bulp" });
+  console.log(delet);
+};
+deleteData();
