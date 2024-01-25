d98d32c65ea6a33e49f2330a396e2657f303e6a8
akashkhot12 <akashkhot03@gmail.com>
2024-01-25 09:19:13 +0530

    create connections

--- Express js/practice/crud/connections3.js
+++ Express js/practice/crud/connections3.js
@@ -1,9 +1,13 @@
 const{MongoClient} = require("mongodb");
+const { collection } = require("../../gettingDataForms/model/patient");
 const url = "";
 const client = new MongoClient(url);
 const database  = "buses";
 
 const connections = async ()=>{
     const connect = await client();
-    const db = 
+    const db = await connect.db(database);
+    return collection.db("bus");
 }
+
+module.exports = connections
